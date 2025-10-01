b048166bc6194c8f004c1b7630140489c4fe034e
Your Name <you@example.com>
2025-10-01 11:33:07 -0700

    Respond fast: EMAIL_MODE + SMTP_TIMEOUT + version tag

---
+++ server/push new files to Github.bat
@@ -0,0 +1,6 @@
+cd C:\PROOFER\proofok
+git status
+git add server
+git commit -m "Sync server updates"
+git pull --rebase origin main
+git push
---
+++ server/server_sinc_github.bat
@@ -0,0 +1,1 @@
+robocopy C:\PROOFER\server C:\PROOFER\proofok\server /E /XF .env /XD uploads data __pycache__
