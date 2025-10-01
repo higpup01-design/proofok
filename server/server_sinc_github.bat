0d871b06f3058f6754e76c90ad893455ba2a44b6
Your Name <you@example.com>
2025-10-01 13:12:05 -0700

    Switch to pure HTML form submit; async email with timeout

--- server/server_sinc_github.bat
+++ server/server_sinc_github.bat
@@ -1,1 +1,7 @@
 robocopy C:\PROOFER\server C:\PROOFER\proofok\server /E /XF .env /XD uploads data __pycache__
+cd C:\PROOFER\proofok
+git add server
+git commit -m "Respond fast: EMAIL_MODE + SMTP_TIMEOUT + version tag"
+git pull --rebase origin main
+git push
+
